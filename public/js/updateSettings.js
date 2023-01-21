32258b3ec2d5c553822a1627538aec7d733930be
osamaahmed0042 <osamaahmedkhan0042@gmail.com>
2023-01-22 00:49:00 +0500

    donw with ssr

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,22 +1,21 @@
-/* eslint-disable */
+
 import axios from 'axios';
 import { showAlert } from './alerts';
 
 // type is either 'password' or 'data'
+
 export const updateSettings = async (data, type) => {
+  console.log(data)
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? 'http://localhost:8000/api/v1/users/updateMyPassword'
+        : 'http://localhost:8000/api/v1/users/updateMe';
 
-    const res = await axios({
-      method: 'PATCH',
-      url,
-      data
-    });
+    const res = await axios.patch(url, data);
 
-    if (res.data.status === 'success') {
+
+    if (res.data.status === 'sucess') {
       showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
